34dd6f63848732f4573f2d83f65aedc1856427ba
AbhijithGanesh <work@abhijithganesh.com>
2024-07-30 20:16:31 +0530

    Changes to the config
    Signed-off-by: AbhijithGanesh <work@abhijithganesh.com>

--- image/app.py
+++ image/app.py
@@ -1,15 +1,25 @@
 import argparse
+import os
+
 import pandas as pd
+
+os.makedirs("submissions", exist_ok=True)
 
 def generate_dummy_csv(output_file):
     dummy_df = pd.DataFrame(
 		{
-			'sample': [1, 2, 3, 4, 5],
-			'feature1': [0.1, 0.2, 0.3, 0.4, 0.5],
-			'feature2': [0.5, 0.4, 0.3, 0.2, 0.1],
-        }
+			"Turning Patterns": [
+				"Car", "Bus", "Truck", "Three-Wheeler", "Two-Wheeler", "LCV", "Bicycle",
+			],
+			"BC": [1, 2, 3, 4, 5, 6, 7],
+			"BE": [1, 2, 3, 4, 5, 6, 7],
+			"DE": [1, 2, 3, 4, 5, 6, 7],
+			"DA":[1, 2, 3, 4, 5, 6, 7],
+			"FA":[9, 10, 10, 0, 0, 0, 0],
+			"FC":[1, 2, 3, 4, 5, 6, 7],
+		}
     )
-    dummy_df.to_csv(output_file, index=False)
+    dummy_df.to_csv(f"submissions/{output_file}", index=False)
 
 def main():
 	parser = argparse.ArgumentParser(description="Process a video file and generate a dummy CSV output.")
